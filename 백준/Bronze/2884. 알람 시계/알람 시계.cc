1ae3bc816104c621aaa6f51db9f3c89e407b5f09
HanJae Kim <126743737+hanjaek@users.noreply.github.com>
2025-04-12 15:18:52 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+
+    // 입력보다 45분 일찍 출력 (h:0~23)(m:0~59)
+    unsigned short hour, minute;
+
+    cin >> hour >> minute;
+
+    
+    if(minute - 45 < 0){
+        minute = abs(60 + (minute - 45));
+        
+        if (hour == 0){
+            hour = 23;
+        }
+        else{
+            hour--;
+        }
+    }
+    else{
+        minute -= 45;
+    }
+
+    cout << hour << " " << minute ;
+}
