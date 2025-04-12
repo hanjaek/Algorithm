0cfc2a153502e1f404290bcb93c4ecbbc336bdef
HanJae Kim <126743737+hanjaek@users.noreply.github.com>
2025-04-12 09:10:39 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    unsigned n;
+    cin >> n;
+
+    for(int i=1; i <= n; i++){
+
+        for(int j=0; j < n-i; j++){
+            cout << " ";
+        }
+
+        for(int k=0; k < i; k++){
+            cout << "*";
+        }
+
+        cout << "\n";
+    }
+
+}
