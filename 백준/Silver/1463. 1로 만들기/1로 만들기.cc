b3c6d9ee76eac840b503d3e6fd2fbff0e75f55ec
HanJae Kim <126743737+hanjaek@users.noreply.github.com>
2025-05-15 16:45:33 +0900

    [Silver III] Title: 1로 만들기, Time: 8 ms, Memory: 5804 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+    int N;
+    cin >> N;
+    
+    int DP[1000001]={0};
+    
+    for(int i=2; i <= N; i++){
+
+        if(i % 6 == 0){
+            int min_dp = min(min(DP[i/3], DP[i/2]), DP[i-1]);
+            DP[i] = min_dp + 1;
+        }
+        else if(i % 3 == 0){
+            DP[i] = min(DP[i/3], DP[i-1]) + 1;
+        }
+        else if(i % 2 ==0){
+            DP[i] = min(DP[i/2], DP[i-1]) + 1;
+        }
+        else{
+            DP[i] = DP[i-1] + 1;
+        }
+    }
+    
+    cout << DP[N];
+
+}
