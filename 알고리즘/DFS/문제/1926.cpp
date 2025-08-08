e89000062a2d6234d851e45f237314fb388913c7
hanjaek <hjk2512@naver.com>
2025-08-08 12:16:17 +0900

    Solved: backjoon 1926

---
+++ 알고리즘/DFS/문제/1926.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+vector<vector<int>> grid;
+vector<vector<bool>> visited;
+
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
+
+void input(){
+    cin >> n >> m;
+    grid.resize(n, vector<int>(m));
+    visited.resize(n, vector<bool>(m, false));
+
+    for(int y=0; y < n; y++){
+        for(int x=0; x < m; x++){
+            cin >> grid[y][x];
+        }
+    }
+}
+
+int dfs(int x, int y){
+    visited[y][x] = true;
+    int cnt = 1;
+
+    for(int dir=0; dir < 4; dir++){
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+
+        if(nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
+        if(!visited[ny][nx] && grid[ny][nx] == 1){
+            cnt += dfs(nx, ny);
+        }
+    }
+
+    return cnt;
+}
+
+void func(){
+    int max_cnt = 0;
+    int areas = 0;
+
+    for(int y=0; y < n; y++){
+        for(int x=0; x < m; x++){
+            if(!visited[y][x] && grid[y][x] == 1){
+                int cnt = dfs(x, y);
+                max_cnt = max(max_cnt, cnt);
+                areas++;
+            }
+        }
+    }
+
+    cout << areas << "\n" << max_cnt;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    input();
+    func();
+}
