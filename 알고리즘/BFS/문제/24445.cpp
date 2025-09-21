a52d08b1656b48fa1b02a5d372db31aff288b4c7
hanjaek <hjk2512@naver.com>
2025-09-22 08:56:16 +0900

    Solved: backjoon 24445

---
+++ 알고리즘/BFS/문제/24445.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int N, M, R;
+vector<vector<int>> E;
+vector<int> visited;
+int seq = 1;
+
+void input(){
+    cin >> N >> M >> R;
+    E.resize(N+1);
+    visited.resize(N+1, 0);
+
+    for(int i=0; i < M; i++){
+        int u, v;
+        cin >> u >> v;
+
+        E[u].push_back(v);
+        E[v].push_back(u);
+    }
+
+    for(int i=1; i <= N; i++){
+        sort(E[i].begin(), E[i].end(), greater<>());
+    }
+}
+
+void bfs(int start){
+    queue<int> q;
+    q.push(start);
+    visited[start] = seq++;
+
+    while(!q.empty()){
+        int cur = q.front();
+        q.pop();
+
+        for(int next : E[cur]){
+            if(visited[next] == 0){
+                q.push(next);
+                visited[next] = seq++;
+            }
+        }
+    }
+}
+
+void func(){
+    bfs(R);
+    for(int i=1; i <= N; i++){
+        cout << visited[i] << "\n";
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    input();
+    func();
+}
