3adc2d234229eba83f8e9595bd0b20e4b80ff424
hanjaek <hjk2512@naver.com>
2025-10-22 15:45:04 +0900

    Solved: backjoon 13023

---
+++ 알고리즘/DFS/문제/13023.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+vector<vector<int>> E;
+vector<bool> visited;
+bool flag = false;
+
+void input(){
+    cin >> N >> M;
+    E.resize(N);
+    visited.resize(N, false);
+
+    for(int i=0; i < M; i++){
+        int a, b;
+        cin >> a >> b;
+
+        E[a].push_back(b);
+        E[b].push_back(a);
+    }
+}
+
+void dfs(int node, int depth){
+    if(flag) return;
+    // depth를 N-1로 했을 때, 시간초과 이유는 ABCDE의 관계만 파악하면 됨.
+    if(depth == 4){
+        flag = true;
+        return;
+    }
+    visited[node] = true;
+
+    for(int next : E[node]){
+        if(!visited[next]){
+            dfs(next, depth+1);
+            if(flag) return;
+        }
+    }
+    visited[node] = false;
+}
+
+void func(){
+    for(int i=0; i < N && !flag; i++){
+        dfs(i, 0);
+    }
+
+    cout << (flag ? 1 : 0) << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    input();
+    func();
+}
