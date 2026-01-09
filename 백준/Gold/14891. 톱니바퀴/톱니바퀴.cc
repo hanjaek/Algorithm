25addd88494c7d899e8cb7b90d2129acbe9b166f
HanJae Kim <126743737+hanjaek@users.noreply.github.com>
2026-01-09 11:14:16 +0900

    [Gold V] Title: 톱니바퀴, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.cc
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.cc
@@ -1,37 +1,38 @@
 #include <iostream>
-#include <vector>
-#include <deque>
 #include <string>
 
 using namespace std;
 
 int N = 4;
 int K;
-deque<int> gear[4];
+int gear[4][8];
+int top_idx[4] = {0, 0, 0, 0};
 
 void input(){
     for(int i=0; i < N; i++){
         string s;
         cin >> s;
-        for(char ch : s){
-            gear[i].push_back(ch -'0');
+        for(int j=0; j < s.size(); j++){
+            gear[i][j] = s[j] - '0';
         }
     }
     cin >> K;
 }
 
+// top_idx 기준 톱니 상태
+int tooth(int idx, int pos){
+    int cur_tooth = gear[idx][(top_idx[idx] + pos) % 8];
+    return cur_tooth;
+}
+
 void rotate_gear(int idx, int dir){
-    // 시계방향
+    // 시계 방향
     if(dir == 1){
-        int back = gear[idx].back();
-        gear[idx].pop_back();
-        gear[idx].push_front(back);
+        top_idx[idx] = (top_idx[idx] - 1 + 8) % 8;
     }
     // 반시계 방향
     else{
-        int front = gear[idx].front();
-        gear[idx].pop_front();
-        gear[idx].push_back(front);
+        top_idx[idx] = (top_idx[idx] + 1) % 8;
     }
 }
 
@@ -40,19 +41,19 @@ void func(){
         int dir[4] = {0, 0, 0, 0};
         int p, rd;
         cin >> p >> rd; p--;
-        
         dir[p] = rd;
-        // 회전 톱니 기준 좌측 톱니들 처리
-        for(int i = p-1; i >= 0; i--){
-            if(gear[i][2] != gear[i+1][6]){
+
+        // 좌측 톱니 처리
+        for(int i=p-1; i >= 0; i--){
+            if(tooth(i, 2) != tooth(i+1, 6)){
                 dir[i] = -dir[i+1];
             }
             else break;
         }
 
-        // 회전 톱니 기준 우측 톱니들 처리
-        for(int i = p+1; i < N; i++){
-            if(gear[i][6] != gear[i-1][2]){
+        // 우측 톱니 처리
+        for(int i=p+1; i < N; i++){
+            if(tooth(i, 6) != tooth(i-1, 2)){
                 dir[i] = -dir[i-1];
             }
             else break;
@@ -60,14 +61,15 @@ void func(){
 
         // 회전 처리
         for(int i=0; i < N; i++){
-            if(dir[i] != 0) rotate_gear(i, dir[i]);
+            if(dir[i] != 0){
+                rotate_gear(i, dir[i]);
+            }
         }
     }
-    
-    // 점수 처리
+    // 계산 처리
     int score = 0;
     for(int i=0; i < N; i++){
-        if(gear[i][0] == 1){
+        if(gear[i][top_idx[i]] == 1){
             score += (1<<i);
         }
     }
