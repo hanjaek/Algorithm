7290a456c0328eba69d9cbb0ee6e106d69c7f044
HanJae Kim <126743737+hanjaek@users.noreply.github.com>
2026-02-28 10:32:13 +0900

    [Silver III] Title: 모든 순열, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+#define endl "\n"
+
+int N;
+vector<int> numbers;
+
+void input(){
+    cin >> N;
+}
+
+void func(){
+    numbers.clear();
+    
+    for(int i=1; i <= N; i++){
+        numbers.push_back(i);
+    }
+
+    do{
+        for(int num : numbers) cout << num << " ";
+        cout << endl;
+    }while(next_permutation(numbers.begin(), numbers.end()));
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    input();
+    func();
+    return 0;
+}
