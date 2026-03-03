cfa69d9ae7e8caa6af2b23eb6e18fcb158b5d5c0
HanJae Kim <126743737+hanjaek@users.noreply.github.com>
2026-03-03 14:31:22 +0900

    [Bronze III] Title: 고급 수학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7510. 고급 수학/고급 수학.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+#define endl "\n"
+
+int N;
+
+bool check_right_triangle(int a, int b, int c){
+    if((a*a + b*b) ==  c*c){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+void input(){
+    cin >> N;
+}
+
+void func(){
+    for(int i=1; i <= N; i++){
+        int arr[3];
+        cin >> arr[0] >> arr[1] >> arr[2];
+
+        sort(arr, arr+3);
+
+        cout << "Scenario #" << i << ":" << endl;
+        cout << (check_right_triangle(arr[0], arr[1], arr[2]) ? "yes" : "no") << endl;
+        cout << endl;
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    input();
+    func();
+}
