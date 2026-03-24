c500c09e8d44daacc2942ffe51eb777de2a8c606
HanJae Kim <126743737+hanjaek@users.noreply.github.com>
2026-03-24 22:07:48 +0900

    [level 2] Title: 구명보트, Time: 1.77 ms, Memory: 4.67 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    int left = 0;
+    int right = people.size() - 1;
+    
+    sort(people.begin(), people.end());
+    
+    while(left <= right){
+        if(people[left] + people[right] <= limit){
+            left++; right--;
+        }
+        else{
+            right--;
+        }
+        answer++;
+    }
+    
+    return answer;
+}
