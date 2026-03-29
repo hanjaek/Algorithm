f471b7fedaf04fdba3c1ef38b1131be1379e71c4
HanJae Kim <hjk2512@naver.com>
2026-03-29 13:37:31 +0900

    [Gold III] Title: 도로 정보, Time: 4 ms, Memory: 2396 KB -BaekjoonHub

--- 백준/Gold/24548. 도로 정보/도로 정보.cc
+++ 백준/Gold/24548. 도로 정보/도로 정보.cc
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <map>
+#include <tuple>
 
 using namespace std;
 #define endl "\n"
@@ -6,10 +8,6 @@ using namespace std;
 
 int N;
 string S;
-
-int get_index(int t, int g, int f, int p){
-    return 27*t + 9*g + 3*f + 1*p;
-}
 
 void input(){
     cin >> N;
@@ -20,8 +18,8 @@ void func(){
     ll result = 0;
     int t=0, g=0, f=0, p=0;
 
-    ll cnt[81] = {0};
-    cnt[0] = 1;
+    map<tuple<int, int, int, int>, ll> cnt;
+    cnt[{t, g, f, p}] = 1;
 
     for(int i=0; i < N; i++){
         char c = S[i];
@@ -31,9 +29,8 @@ void func(){
         else if(c == 'F') f = (f+1)%3;
         else p = (p+1)%3;
 
-        int idx = get_index(t, g, f, p);
-        result += cnt[idx];
-        cnt[idx]++;
+        result += cnt[{t, g, f, p}];
+        cnt[{t, g, f, p}]++;
     }
     cout << result << endl;
 }
