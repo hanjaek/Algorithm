565abfde3c0ef17d54182f111e1ed4161295294a
HanJae Kim <hjk2512@naver.com>
2026-04-02 15:20:28 +0900

    [level 2] Title: 올바른 괄호, Time: 0.13 ms, Memory: 3.85 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -1,22 +1,31 @@
 #include <iostream>
-#include <stack>
+// #include <stack>
 
 using namespace std;
 
 bool solution(string s)
 {
-    bool answer = true;
-    stack<char> st;
+//     bool answer = true;
+//     stack<char> st;
     
-    for(char c : s){
-        if(c == ')'){
-            if(st.empty()) return false;
-            st.pop();
-        }else{
-            st.push(c);
-        }
+//     for(char c : s){
+//         if(c == ')'){
+//             if(st.empty()) return false;
+//             st.pop();
+//         }else{
+//             st.push(c);
+//         }
+//     }
+//     if(!st.empty()) answer = false;
+    
+//     return answer;
+// =========== 다른 풀이 ===========
+    int n = 0;
+    for(int i=0; i < s.size(); i++){
+        if(n < 0) return false;
+        
+        if(s[i] == '(') n++;
+        else n--;
     }
-    if(!st.empty()) answer = false;
-    
-    return answer;
+    return n == 0;
 }
