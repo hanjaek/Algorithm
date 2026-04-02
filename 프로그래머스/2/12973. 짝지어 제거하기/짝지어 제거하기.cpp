f14345f8955ab6fdf9870c984ab452a9fcf11b52
HanJae Kim <hjk2512@naver.com>
2026-04-02 19:15:06 +0900

    [level 2] Title: 짝지어 제거하기, Time: 3.95 ms, Memory: 5.99 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
-#include <stack>
+// #include <stack>
 using namespace std;
 
 int solution(string s)
 {
-    stack<char> st;
+//     stack<char> st;
+    
+//     for(char c : s){
+//         if(!st.empty() && st.top() == c){
+//             st.pop();
+//         }
+//         else st.push(c);
+//     }
+    
+//     return (st.empty() ? 1 : 0);
+    
+//  ========== 다른 풀이 ==========
+    int idx = 0;
     
     for(char c : s){
-        if(!st.empty() && st.top() == c){
-            st.pop();
+        if(idx > 0 && s[idx-1] == c){
+            idx--;
         }
-        else st.push(c);
+        else{
+            s[idx] = c;
+            idx++;
+        }
     }
-    
-    return (st.empty() ? 1 : 0);
+    return !idx;
 }
