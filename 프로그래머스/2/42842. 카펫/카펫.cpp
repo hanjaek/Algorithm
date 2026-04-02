bf21972aca24410b3bd9835d5a5272653f0a0e35
HanJae Kim <hjk2512@naver.com>
2026-04-02 19:44:17 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -1,18 +1,15 @@
-#include <string>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
 vector<int> solution(int brown, int yellow) {
     int total = brown + yellow;
     
-    for(int i=3; i < total; i++){
-        for(int j=3; j <= i; j++){
-            if(i*j == total){
-                if((i-2)*(j-2) == yellow){
-                    return {i, j};
-                }
-            }
-        }
+    for(int h=3; h <= sqrt(total); h++){
+        if(total % h != 0) continue;
+        
+        int w = total / h;
+        if((w-2)*(h-2) == yellow) return {w, h};
     }
 }
