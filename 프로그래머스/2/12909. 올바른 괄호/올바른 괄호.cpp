0cd948d3cb498581b6dc2b40cb91c4b388f0e570
HanJae Kim <hjk2512@naver.com>
2026-04-02 15:16:08 +0900

    [level 2] Title: 올바른 괄호, Time: 0.40 ms, Memory: 3.85 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    stack<char> st;
+    
+    for(char c : s){
+        if(c == ')'){
+            if(st.empty()) return false;
+            st.pop();
+        }else{
+            st.push(c);
+        }
+    }
+    if(!st.empty()) answer = false;
+    
+    return answer;
+}
