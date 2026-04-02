49464017b6003b08f6eb5c90c42412ac821d06b8
HanJae Kim <hjk2512@naver.com>
2026-04-02 18:51:38 +0900

    [level 2] Title: 짝지어 제거하기, Time: 6.41 ms, Memory: 6.35 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int solution(string s)
+{
+    stack<char> st;
+    
+    for(char c : s){
+        if(!st.empty() && st.top() == c){
+            st.pop();
+        }
+        else st.push(c);
+    }
+    
+    return (st.empty() ? 1 : 0);
+}
