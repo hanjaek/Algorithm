3daac5f922eb6000979c5259be029c4c706f79c3
HanJae Kim <hjk2512@naver.com>
2026-04-07 15:54:45 +0900

    [level 2] Title: 할인 행사, Time: 120.08 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
@@ -12,18 +12,20 @@ bool check_item(vector<string> want, vector<int> number, unordered_map<string, i
 }
 
 int solution(vector<string> want, vector<int> number, vector<string> discount) {
-    int need_day = 0;
     int answer = 0;
+    unordered_map<string, int> m;
     
-    for(int num : number) need_day += num;
-    for(int i = 0; i <= discount.size() - need_day; i++){
-        unordered_map<string, int> m;
-        for(int j = i; j < i + need_day; j++){
-            m[discount[j]]++;
-        }
-        if(check_item(want, number, m)){
-            answer++;
-        }
+    for(int i=0; i < 10; i++){
+        m[discount[i]]++;
     }
+    if(check_item(want, number, m)) answer++;
+    
+    for(int i=10; i < discount.size(); i++){
+        m[discount[i-10]]--;
+        m[discount[i]]++;
+        
+        if(check_item(want, number, m)) answer++;
+    }
+    
     return answer;
 }
