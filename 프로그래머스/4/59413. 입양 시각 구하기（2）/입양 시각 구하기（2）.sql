8440db7a62ad3a45893286e550f33c5fc05b37e4
HanJae Kim <hjk2512@naver.com>
2026-05-08 18:52:29 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -0,0 +1,11 @@
+-- 코드를 입력하세요
+SET @HOUR = -1;
+
+SELECT (@HOUR := @HOUR + 1) as HOUR,
+    (
+        SELECT COUNT(*)
+        FROM ANIMAL_OUTS
+        WHERE HOUR(DATETIME) = @HOUR
+    ) as COUNT
+FROM ANIMAL_OUTS
+WHERE @HOUR < 23
